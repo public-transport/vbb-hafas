e6cdf3a61927d356d7fbc03c23a25fa2b45d1415
Jannis R <mail@jannisr.de>
2015-07-24 15:38:17 +0200

    WIP; `stations.departures`

--- src/services/stations.js
+++ src/services/stations.js
@@ -1,4 +1,5 @@
-//extend =		require('extend');
+extend =		require('extend');
+sDate =			require('s-date');
 
 
 
@@ -20,8 +21,61 @@ var stations = module.exports = {
 	},
 
 
+	_departuresDefaults: {
+		results:	10,
+		when:		null,
+		direction:	null,
+		products: {
+			suburban:	true,
+			subway:		true,
+			tram:		true,
+			bus:		true,
+			ferry:		true,
+			express:	true,
+			regional:	true
+		}
+	},
 
-	// todo
+	// returns a promise
+	departures: function (id, options) {
+		if (!id) throw new Error('Missing `id` parameter.');
+
+		options = extend(true, {}, this._departuresDefaults, options || {});
+		if (!options.when) options.when = new Date();   // now
+
+		params = {
+			id:				id,
+			maxNo:			options.results,
+			date:			sDate('{yyyy}-{mm}-{dd}', options.when),
+			time:			sDate('{hh24}:{Minutes}', options.when),
+			products:		products.typesToNumber(options.products)
+		};
+		if (options.direction) params.direction = options.direction;
+
+		return this.client._request('departureBoard', params)
+		.then(this._departuresOnSuccess, console.error);   // remove `console` parts
+	},
+
+	_departuresOnSuccess: function (data) {
+		var results = [];
+		var i, length, departure;
+
+		if (data.Departure)
+			for (i = 0, length = data.Departure.length; i < length; i++) {
+				departure = data.Departure[i];
+				// todo: what is `departure.type`?
+				results.push({
+					name:		departure.Product.name,
+					line:		departure.Product.line,
+					direction:	departure.direction,
+					product:	products[departure.trainCategory].type,
+					when:		new Date(departure.date + ' ' + departure.time),
+					realtime:	new Date(departure.rtDate + ' ' + departure.rtTime)
+				});
+			}
+
+		return results;
+	}
 
 
 
