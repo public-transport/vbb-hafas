0b026f89b5d2d0da61d85dcd8391b7c1af8020dc
Jannis R <mail@jannisr.de>
2015-07-24 17:37:13 +0200

    WIP; `stations.arrivals`

--- src/services/stations.js
+++ src/services/stations.js
@@ -21,8 +21,18 @@ var stations = module.exports = {
 	},
 
 
-	_departuresDefaults: {
-		results:	10,
+
+	arrivals: function (station, options) {
+		return this._request('arrivalBoard', this._arrivalsOnSuccess, station, options);
+	},
+	departures: function (station, options) {
+		return this._request('departureBoard', this._departuresOnSuccess, station, options);
+	},
+
+
+
+	_requestDefaults: {
+		results:	3,   // todo: change to 10
 		when:		null,
 		direction:	null,
 		products: {
@@ -37,23 +47,45 @@ var stations = module.exports = {
 	},
 
 	// returns a promise
-	departures: function (id, options) {
-		if (!id) throw new Error('Missing `id` parameter.');
+	_request: function (service, handler, station, options) {
+		if (!service) throw new Error('Missing `service` parameter.');
+		if (!handler) throw new Error('Missing `handler` parameter.');
+		if (!station) throw new Error('Missing `station` parameter.');
 
-		options = extend(true, {}, this._departuresDefaults, options || {});
+		options = extend(true, {}, this._requestDefaults, options || {});
 		if (!options.when) options.when = new Date();   // now
 
 		params = {
-			id:				id,
-			maxNo:			options.results,
+			id:				station,
+			maxJourneys:	options.results,
 			date:			sDate('{yyyy}-{mm}-{dd}', options.when),
 			time:			sDate('{hh24}:{Minutes}', options.when),
 			products:		products.typesToNumber(options.products)
 		};
 		if (options.direction) params.direction = options.direction;
 
-		return this.client._request('departureBoard', params)
-		.then(this._departuresOnSuccess, console.error);   // remove `console` parts
+		return this.client._request(service, params)
+		.then(handler, console.error);   // todo: remove `console.error`
+	},
+
+	_arrivalsOnSuccess: function (data) {
+		var results = [];
+		var i, length, departure;
+
+		if (data.Arrival)
+			for (i = 0, length = data.Arrival.length; i < length; i++) {
+				departure = data.Arrival[i];
+				// todo: what is `departure.type`?
+				results.push({
+					name:		departure.Product.name,
+					line:		departure.Product.line,
+					product:	products[departure.trainCategory].type,
+					when:		new Date(departure.date + ' ' + departure.time),
+					realtime:	new Date(departure.rtDate + ' ' + departure.rtTime)
+				});
+			}
+
+		return results;
 	},
 
 	_departuresOnSuccess: function (data) {
