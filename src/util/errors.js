44114c7767f460ae79b6c70b1ed0232a431e7a4b
Jannis R <mail@jannisr.de>
2015-08-01 14:54:12 +0200

    WIP; better error handling

--- src/util/errors.js
+++ src/util/errors.js
@@ -8,29 +8,7 @@ var e = module.exports = {};
 
 
 
-e.RequestError = function (code, message, url) {
-	Error.call(this, message);
-	this.code =			code;
-	this.message =		message;
-	this.url =			url;
-};
-
-e.RequestError.prototype = Object.create(Error.prototype);
-e.RequestError.prototype.constructor = e.RequestError;
-
-e.RequestError.prototype.toString = function () {
-	return [
-		'request error: ',
-		this.code,
-		'–',
-		this.message,
-		'(' + this.url + ')'
-	].join(' ');
-};
-
-
-
-e.HttpError = function (code, message, url, method) {
+e.ConnectionError = function (code, message, url, method) {
 	Error.call(this, message);
 	this.code =			code;
 	this.message =		message;
@@ -38,17 +16,11 @@ e.HttpError = function (code, message, url, method) {
 	this.method =		method;
 };
 
-e.HttpError.prototype = Object.create(Error.prototype);
-e.HttpError.prototype.constructor = e.HttpError;
+e.ConnectionError.prototype = Object.create(Error.prototype);
+e.ConnectionError.prototype.constructor = e.ConnectionError;
 
-e.HttpError.prototype.toString = function () {
-	return [
-		'HTTP error: ',
-		this.code,
-		'–',
-		this.method,
-		this.url
-	].join(' ');
+e.ConnectionError.prototype.toString = function () {
+	return 'connection error: ' + this.code + ' ' + this.message;
 };
 
 
@@ -82,19 +54,4 @@ e.apiServerError = function (res, data) {
 	var error = group[parseInt(data.errorCode.substr(1))];
 	if (!error) return unknownError;
 	return new e.ApiServerError(group.name, error.code, error.message, res.request.url, data.errorText);
-};
-
-
-
-e.fromRequestError = function (err) {
-	return new e.RequestError(err.error.code, err.message, err.options.uri);
-};
-
-e.fromStatusCodeError = function (err) {
-	try {
-		var data = JSON.parse(err.message);
-		return e.apiServerError(err.response, data);
-	} catch (e) {
-		return new e.HttpError(err.response.statusCode, err.response.statusMessage, err.options.uri, err.response.request.method);
-	}
 };
