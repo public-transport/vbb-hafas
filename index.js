d771116a24d75cc8090c26700e7078f6563e64d3
Jannis R <mail@jannisr.de>
2016-05-25 17:17:51 +0200

    clean slate

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict'
 
-const hafas = (cache) => ({
-	  routes:     require('./lib/routes')(cache)
-	, departures: require('./lib/departures')(cache)
-})
-
-const redisCache = require('./lib/cache')
-module.exports = Object.assign(hafas(redisCache), {hafas})
+module.exports = {
+	routes: require('./routes')
+}
