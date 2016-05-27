6bfe2da2d010519bafd74f3111974d7e51e0a821
Jannis R <mail@jannisr.de>
2016-05-27 19:54:10 +0200

    minor stuff

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
 module.exports = {
-	routes: require('./lib/routes')
+	  routes:     require('./lib/routes')
+	, departures: require('./lib/departures')
+	, nearby:     require('./lib/nearby')
 }
