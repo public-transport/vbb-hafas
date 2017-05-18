ac9a1f84c8584979057d09e92c5b4bb9f0dfc446
Jannis R <mail@jannisr.de>
2017-05-18 13:18:30 +0200

    example :memo:, update deps, 2.0.0

---
+++ example.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const hafas = require('.')
+
+hafas.journeys('900000003201', '900000024101', {results: 1})
+// hafas.departures('900000013102', {duration: 1})
+// hafas.locations('Alexanderplatz', {results: 2})
+// hafas.nearby(52.5137344, 13.4744798, {distance: 60})
+// hafas.radar(52.52411, 13.41002, 52.51942, 13.41709, {results: 10})
+.then((data) => {
+	console.log(require('util').inspect(data, {depth: null}))
+}, console.error)
