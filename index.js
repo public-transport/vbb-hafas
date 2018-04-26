302a5a385e558fa49377faf95ca1bd87f5d55c27
Jannis R <mail@jannisr.de>
2018-04-26 22:08:32 +0200

    add transfer info to journeys

--- index.js
+++ index.js
@@ -3,6 +3,23 @@
 const createClient = require('hafas-client')
 const vbbProfile = require('hafas-client/p/vbb')
 
+const addTransferInfoToJourney = require('./lib/add-transfer-info')
+
 const client = createClient(vbbProfile)
+
+const origJourneys = client.journeys
+const journeysWithTransfers = (from, to, opt = {}) => {
+	if (opt && opt.transferInfo) opt.passedStations = true
+	const p = origJourneys(from, to, opt)
+
+	if (opt && opt.transferInfo) {
+		return p.then((journeys) => {
+			for (let journey of journeys) addTransferInfoToJourney(journey)
+			return journeys
+		})
+	}
+	return p
+}
+client.journeys = journeysWithTransfers
 
 module.exports = client
