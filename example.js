5a401e4564dd750f73d289ef86ff3537c96370b5
Jannis R <mail@jannisr.de>
2018-05-16 22:31:26 +0200

    5.0.0-alpha.3

--- example.js
+++ example.js
@@ -22,7 +22,12 @@ hafas.journeys(spichernstr, bismarckstr, {
 // hafas.locations('Alexanderplatz', {results: 2})
 // hafas.location('900000017104')
 // hafas.nearby(52.5137344, 13.4744798, {distance: 60})
-// hafas.radar(52.52411, 13.41002, 52.51942, 13.41709, {results: 10})
+// hafas.radar({
+// 	north: 52.52411,
+// 	weat: 13.41002,
+// 	south: 52.51942,
+// 	east: 13.41709
+// }, {results: 10})
 .then((data) => {
 	console.log(require('util').inspect(data, {depth: null}))
 })
