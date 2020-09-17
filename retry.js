c6db5f52a75b1fdade06a0120f5871aba0475226
Jannis R <mail@jannisr.de>
2020-09-17 19:39:10 +0200

    fix /retry & /throttle 🐛

--- retry.js
+++ retry.js
@@ -4,8 +4,11 @@ const createClient = require('hafas-client')
 const withRetrying = require('hafas-client/retry')
 const vbbProfile = require('hafas-client/p/vbb')
 
-const createRetryingClient = (userAgent, retryOpts = {}) => {
-	return withRetrying(createClient, retryOpts)(vbbProfile, userAgent)
+const createRetryingClient = (userAgent, opt = {}) => {
+	const {retryOpts} = {retryOpts: {}, ...opt}
+
+	const retryingProfile = withRetrying(vbbProfile, retryOpts)
+	return createClient(retryingProfile, userAgent, opt)
 }
 
 module.exports = createRetryingClient
--- throttle.js
+++ throttle.js
@@ -4,8 +4,18 @@ const createClient = require('hafas-client')
 const withThrottling = require('hafas-client/throttle')
 const vbbProfile = require('hafas-client/p/vbb')
 
-const createThrottledClient = (userAgent, limit = 5, interval = 1000) => {
-	return withThrottling(createClient, limit, interval)(vbbProfile, userAgent)
+const createThrottledClient = (userAgent, opt = {}) => {
+	const {
+		throttlingLimit: limit,
+		throttlingInterval: interval
+	} = {
+		throttlingLimit: 5,
+		throttlingInterval: 1000, // 1s
+		...opt
+	}
+
+	const throttledProfile = withThrottling(vbbProfile, limit, interval)
+	return createClient(throttledProfile, userAgent, opt)
 }
 
 module.exports = createThrottledClient
