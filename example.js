6f30023be9d4491e9fb7c59e45499fbc8a4e13c6
Jannis R <mail@jannisr.de>
2022-11-29 15:16:27 +0100

    example: use top-level await 📝

--- example.js
+++ example.js
@@ -10,36 +10,38 @@ const hafas = createHafas('vbb-hafas-example')
 const spichernstr = '900000042101'
 const bismarckstr = '900000024201'
 
-hafas.journeys(spichernstr, bismarckstr, {
+let data = await hafas.journeys(spichernstr, bismarckstr, {
 	results: 1,
 	tickets: true,
 	stopovers: true,
 	transferInfo: true,
 	polylines: true,
 })
-// .then(({journeys}) => {
-// 	const [journey] = journeys
-// 	return hafas.refreshJourney(journey.refreshToken, {
+
+// {
+// 	const [journey] = data.journeys
+// 	data = await hafas.refreshJourney(journey.refreshToken, {
 // 		stopovers: true,
 // 		transferInfo: true,
 // 	})
-// })
-// .then(({journeys}) => {
-// 	const [journey] = journeys
+// }
+
+// {
+// 	const [journey] = data.journeys
 // 	const leg = journey.legs[0]
-// 	return hafas.trip(leg.tripId, leg.line.name, {polyline: true})
-// })
+// 	data = await hafas.trip(leg.tripId, leg.line.name, {polyline: true})
+// }
 
-// hafas.departures('spichernstr', {duration: 1})
-// hafas.arrivals('spichernstr', {duration: 10, linesOfStops: true})
+// let data = await hafas.departures('spichernstr', {duration: 1})
+// let data = await hafas.arrivals('spichernstr', {duration: 10, linesOfStops: true})
 
-// hafas.locations('spichernstr', {results: 2})
-// hafas.nearby({
+// let data = await hafas.locations('spichernstr', {results: 2})
+// let data = await hafas.nearby({
 // 	type: 'location',
 // 	latitude: 52.5137344,
 // 	longitude: 13.4744798
 // }, {distance: 60})
-// hafas.reachableFrom({
+// let data = await hafas.reachableFrom({
 // 	type: 'location',
 // 	address: '13353 Berlin-Wedding, Torfstr. 17',
 // 	latitude: 52.541797,
@@ -48,19 +50,13 @@ hafas.journeys(spichernstr, bismarckstr, {
 // 	when: new Date('2018-08-27T10:00:00+0200'),
 // 	maxDuration: 10
 // })
-// hafas.stop(spichernstr, {linesOfStops: true})
+// let data = await hafas.stop(spichernstr, {linesOfStops: true})
 
-// hafas.radar({
+// let data = await hafas.radar({
 // 	north: 52.52411,
 // 	west: 13.41002,
 // 	south: 52.51942,
 // 	east: 13.41709
 // }, {results: 10})
 
-.then((data) => {
-	console.log(require('util').inspect(data, {depth: null, colors: true}))
-})
-.catch((err) => {
-	console.error(err)
-	process.exitCode = 1
-})
+console.log(inspect(data, {depth: null, colors: true}))
